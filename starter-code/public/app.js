065ebb371353c950a30591de6b881e40e08194b7
snopeks <creativesnopeks@gmail.com>
2017-10-09 22:52:41 -0700

    add logic for server and app routing

--- starter-code/public/app.js
+++ starter-code/public/app.js
@@ -1,4 +1,46 @@
 console.log('app.js connected');
 $(document).ready(function(){
   console.log('DOM ready');
+
+  $('#guess-number-form').submit(function( event ){
+    event.preventDefault();
+    let formIn = $('#guess-number-form').serialize();
+    console.log(`http://localhost:3000/api/pickanumber?${formIn}`)
+    $.ajax({
+        method: 'GET',
+        url: `http://localhost:3000/api/pickanumber?${formIn}`,
+        success: handleSuccess,
+        error: handleError
+    });
+    console.log('submitted guess number form')
+  });
+
+  $('#target-number-form').submit(function( event ){
+    event.preventDefault();
+    let formIn = $('#target-number-form').serialize();
+    console.log(`http://localhost:3000/api/set-a-number?${formIn}`)
+    $.ajax({
+      method:'GET',
+      url: `http://localhost:3000/api/set-a-number?${formIn}`,
+      success: handleTargetSuccess,
+      error: handleTargetError
+    });
+    console.log('submitted target number form')
+  });
 });
+
+function handleSuccess(responseData){
+  console.log(responseData);
+}
+
+function handleError(xhr, status, errorThrown){
+  console.log('uh oh, error');
+}
+
+function handleTargetSuccess(target){
+  console.log(target)
+}
+
+function handleTargetError(xhr, status, errorThrown){
+  console.log('target not set! error!')
+}
--- starter-code/server.js
+++ starter-code/server.js
@@ -3,8 +3,10 @@
 
 var express = require('express');
 var app = express();
+var path = require('path');
 
-
+app.use('/', express.static(__dirname + '/views'));
+// app.use('/views', express.static(__dirname + '/views'));
 // MIDDLEWARE
 app.use(express.static('public'));
 
@@ -22,16 +24,60 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // ROUTES
 // Root Route
+app.get('/', function(req, res){
+  res.sendFile('/index.html')
+});
 
-
+app.get('/artgallery', function(req, res){
+  console.log(__dirname)
+  var filePath = path.join(__dirname, "/views/art-gallery.html")
+  res.sendFile(filePath)
+})
 // Gallery View Route
 
-
 // The Number Guessing Game
+app.get('/api/pickanumber', function(req, res){
+  // var targetNum = 30;
+  var numGuess = parseInt(req.query.number)
+  if(numGuess < setTarget){
+    res.send('too low!')
+  }
+  else if(numGuess > setTarget){
+    res.send(`too high!`)
+  }
+  else if (numGuess === setTarget){
+    res.send(`you guessed the target num of ${setTarget}! Well done!`)
+  }
+});
 
+var setTarget = 0;
+app.get('/api/set-a-number', function (req, res){
+  setTarget = parseInt(req.query.number)
+  res.send('target set')
+});
+
+app.get('/api/gallery', function(req, res){
+  res.json(gallery)
+});
+app.post('/api/newartwork', function(req, res){
+  console.log("post url")
+  gallery.push(req.body)
+  res.json(gallery)
+})
 
 // Gallery
-
+var gallery = [
+  {
+    title: "Morning Fire",
+    artist: "Varys",
+    description: "Orange clouds in sunrise"
+  },
+  {
+    title: "Dragon Tooth",
+    artist: "D. Targaryan",
+    description: "A fallen dragon tooth in the grass"
+  }
+]
 
 // SERVER START
 var port = 3000;
